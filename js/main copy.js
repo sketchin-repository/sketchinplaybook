3e10691ef7a042cc8174c269f605c4b200cd8a7f
Roberto Falcone <robertofalcone@macbook-pro-di-roberto.home>
2015-08-27 10:48:41 +0200

    fixed TOC, lazyLoading, added scrollToTop

--- js/main copy.js
+++ js/main copy.js
@@ -1,20 +1,17 @@
-function setIdToHeadings() {
+$(document).ready(function() {
+
     // Give id to each title
     $('h2, h3, h4').each(function() {
         var _text = $(this).text();
         var _parsedId = _text.replace(/ /g, "-").toLowerCase();
         $(this).attr("id", _parsedId);
     });
-}
 
-function tableOfContents() {
     // Initialize Table of Contents
     $("#wrapper").tableofcontents({
         id: "#tableofcontents"
     });
-}
 
-function smoothScrolling() {
     // Smooth scrolling anchor links
     $("#tableofcontents li a").click(function() {
         var target = $(this).attr("href");
@@ -22,17 +19,19 @@ function smoothScrolling() {
             scrollTop: $(target).offset().top - 100
         }, 1000);
     });
-}
 
-function scrollToTop() {
+    // Initialize images lazy loading 
+    $("img").lazyload();
+
     //scroll to top
+    function scrollToTop() {
       var _scrollTrigger = $('#scrollToTop'),
           _scrollDestination = $('#tableofcontents'),
           _scrollValue = _scrollDestination.offset().top;
 
       $(window).scroll(function() {
           //Check to see if the window is on top otherwise display the button
-          if ($(this).scrollTop() > _scrollValue + 100) {
+          if ($(this).scrollTop() > _scrollValue + 50) {
             $('#scrollToTop').fadeIn('slow');
           } else {
             $('#scrollToTop').fadeOut('slow');
@@ -45,17 +44,8 @@ function scrollToTop() {
           $('html, body').animate({scrollTop : 0},1000);
           return false;
         });
-}
+    }
 
-function lazyLoading() {
-    // Initialize images lazy loading 
-     $("#content img").lazyload();
-}
+    scrollToTop(); 
 
-$(document).ready(function() {
-    setIdToHeadings();
-    tableOfContents();
-    smoothScrolling();
-    scrollToTop();
-    lazyLoading(); 
 });
